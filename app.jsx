52e8503e70db5d14a73a952d0d5b81d98a8741ec
madisonquanisha <madisonquanisha11@gmail.com>
2018-05-06 22:25:03 -0400

    adding giles

---
+++ app.jsx
@@ -0,0 +1,61 @@
+function Header(props) {
+  return(<div className="scoreboard">
+    <div className="header">
+    <h1> {props.title} </h1>
+    </div>
+  );
+}
+Header.propTypes =    {
+  title: React.propTypes.string.isRequired,
+}
+
+};
+
+function Player(props) {
+  return(<div className="player">
+    <div className="player-name"> {props.name} </div>
+    <div className="player-score">
+    <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <div className="counter-score"> {props.score}</div>
+    <button className="counter-action increment"> + </button>
+    </div> 
+    </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name:React.PropTypes.string.isRequired,
+  score:React.PropTypes.number.isRequired,
+  
+};
+function Application(props) {
+  return (
+    <div className="scoreboard">
+    <div className="header">
+    <h1> {props.title} </h1>
+    </div>
+    <div className="players">
+    <div className="player">
+    <div className="player-name"> Quanisha Madison </div>
+    <div className="player-score">
+    <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <div className="counter-score"> 31</div>
+    <button className="counter-action increment"> + </button>
+    </div> 
+    </div>
+    </div>
+    </div> 
+    </div>
+    
+  );
+}
+Application.propTypes = {
+  title: React.Proptypes.string,
+};
+Application.defaultProps = {
+  title: "Scoreboard",
+}
+ReactDOM.render(<Application />, document.getElementById('container'));
